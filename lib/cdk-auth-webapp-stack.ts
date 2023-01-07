6c4a4c9b46f9e3dfc3a0b231eae94ff0333c5a49
Luca Reccia <lucareccia@hotmail.it>
2023-01-07 18:31:40 +0100

    updated with .env

--- lib/cdk-auth-webapp-stack.ts
+++ lib/cdk-auth-webapp-stack.ts
@@ -1,14 +1,4 @@
-import { CloudFormationCollection } from './../node_modules/aws-sdk/clients/devopsguru.d';
-import { DomainPrefix } from './../node_modules/aws-sdk/clients/amplify.d';
-import { OriginAccessControl, OriginAccessControlConfig } from './../node_modules/aws-sdk/clients/cloudfront.d';
-import { OAuthScopesElement } from './../node_modules/aws-sdk/clients/amplifybackend.d';
-import { OAuthScope } from './../node_modules/aws-sdk/clients/appflow.d';
-import { scope } from './../node_modules/aws-sdk/clients/ec2.d';
-import { ClientSecret, CognitoUserPool } from './../node_modules/aws-sdk/clients/sagemaker.d';
-import { CfnParameter, Duration, lambda_layer_awscli, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnParameter, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
@@ -18,23 +8,15 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deployment from 'aws-cdk-lib/aws-s3-deployment';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as iam from 'aws-cdk-lib/aws-iam';
-import { Asset } from 'aws-cdk-lib/aws-s3-assets';
-
-
-
-//TODO: make the script region agnostic
-//TODO: fare in modo che siano in modo 
-//TODO: make sure that on destoy everything is completely obliterated
-//TODO: pulire tutti gli import
-//TODO: aggiungere tutti i commenti
-//TODO: migliorare il frontends
-//TODO: delete the costants file
-//TODO: destroy cognito
 
 export class CdkAuthWebappStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    /**
+     * INIT CONFIGURATION
+     * setting up required paramters and checking their value
+     */
     const domainPrefixParam = new CfnParameter(this, 'DomainPrefix', {
       type: 'String',
       description: 'You have to set it in google cloud as well', //(TODO: add link to explain properly)
@@ -59,8 +41,15 @@ export class CdkAuthWebappStack extends Stack {
       throw new Error('Make sure you initialized DomainPrefix, GoogleClientId and GoogleClientSecret in the stack parameters')
     }
 
-    const s3frontend = new s3.Bucket(this, 'Bucket', {
-      bucketName: domainPrefixParam.valueAsString+'-frontend-bucket',
+
+
+    /**
+     * BUCKET FOR FRONTEND CREATION
+     */
+
+    // This will create the bucket that stores the frontend code
+    const frontendbucket = new s3.Bucket(this, 'FrontendBucket', {
+      bucketName: `${domainPrefixParam.valueAsString}-frontend-bucket`,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       encryption: s3.BucketEncryption.S3_MANAGED,
       enforceSSL: true,
@@ -70,16 +59,25 @@ export class CdkAuthWebappStack extends Stack {
       websiteIndexDocument: "index.html",
     }); 
 
-    //TODO: fare in modo che questa origin access identity non sia legacy quando deployo
+
+
+    /**
+     * CLOUDFRONT DISTRIBUTION
+     * this is sadly used only to easily give https access to the website as it is required by cognit
+     */
+
+    // This is used to give the CF distribution access to the bucket
+    // TODO: IDK why it says "legacy", I need to make it select the other
     const cfdistributionoriginaccessidentity = new cloudfront.OriginAccessIdentity(this, 'CFOriginAccessIdentity', {
       comment: "Used to give bucket read to cloudfront"
     })
 
-    const cfdistribution = new cloudfront.CloudFrontWebDistribution(this, 'CFDistributionFrontend', {
+    // This is used to create the actual cf distribution
+    const cfdistribution = new cloudfront.CloudFrontWebDistribution(this, 'CFDistribution', {
       originConfigs: [
         {
           s3OriginSource: {
-            s3BucketSource: s3frontend,
+            s3BucketSource: frontendbucket,
             originAccessIdentity: cfdistributionoriginaccessidentity
           },
           behaviors: [{ 
@@ -97,20 +95,37 @@ export class CdkAuthWebappStack extends Stack {
       ]
     })
 
-    s3frontend.grantRead(cfdistributionoriginaccessidentity)
+    // This is also used to give the CF distribution access to the bucket
+    frontendbucket.grantRead(cfdistributionoriginaccessidentity)
 
-    const cfdistributionpolicy = new iam.PolicyStatement({
+    // This is used by cognito to allow list URL that the Hosted UI can redirect to
+    const CLOUDFRONT_PUBLIC_URL = `https://${cfdistribution.distributionDomainName}/`
+
+    /*
+    // This is used to give CloudFormation permission to invalidate a cf distribution
+    //TODO: probably to delete
+    new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
       actions: ['cloudfront:CreateInvalidation'],
       resources: [`"arn:aws:cloudfront::${this.account}:distribution/${cfdistribution.distributionId}"`]
     });
+    */
 
-    const userpool = new cognito.UserPool(this, 'WebAppUserPool', {
+
+
+    /**
+     * COGNITO USER POOL CREATION
+     */
+
+    // The userpool will register all the users that ever logged in
+    const userpool = new cognito.UserPool(this, 'UserPool', {
       userPoolName: 'web-app-user-pool',
-      selfSignUpEnabled: false
+      selfSignUpEnabled: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
     })
 
-    const userpoolidentityprovidergoogle = new cognito.UserPoolIdentityProviderGoogle(this, 'WebAppUserPoolIdentityGoogle', {
+    // This integrates the existing user pool with Google
+    const identityprovidergoogle = new cognito.UserPoolIdentityProviderGoogle(this, 'IdentityProviderGoogle', {
       clientId: googleClientIdParam.valueAsString,
       clientSecret: googleClientSecretParam.valueAsString,
       userPool: userpool,
@@ -120,15 +135,14 @@ export class CdkAuthWebappStack extends Stack {
       scopes: [ 'email' ]
     })
 
-    // this is used to make the hostedui reachable
+    // This will create the Hosted UI URL
     userpool.addDomain('Domain', {
       cognitoDomain: {
         domainPrefix: domainPrefixParam.valueAsString
       }
     })
 
-    const CLOUDFRONT_PUBLIC_URL = `https://${cfdistribution.distributionDomainName}/`
-
+    // This will create the client associated with the user pool and the identity provider
     const client = userpool.addClient('Client', {
       oAuth: {
         flows: {
@@ -151,18 +165,34 @@ export class CdkAuthWebappStack extends Stack {
       ]
     })
 
-    client.node.addDependency(userpoolidentityprovidergoogle)
+    client.node.addDependency(identityprovidergoogle)
 
-    // defines an AWS Lambda resource
-    const securedlambda = new lambda.Function(this, 'AuhtorizedRequestsHandler', {
+
+
+    /**
+     * BACKEND LAMBDA
+     * We will set it up in such a way that only logged users can access it
+     */
+
+    // This defines the lambda itself
+    const securedlambda = new lambda.Function(this, 'SecuredLambda', {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'secured.handler'
     });
+    
+    // This create an integration to import the lambda in the apigw
+    const lambdaapigwintegration = new apigw.LambdaIntegration(securedlambda)
 
-    const lambdaapiintegration = new apigw.LambdaIntegration(securedlambda)
 
-    const backendapigw = new apigw.RestApi(this, 'AuthorizedRequestAPI', {
+
+    /**
+     * BACKEND APIGateway
+     * We will set it up in such a way that only logged users can access it
+     */
+
+    // This defines the apigw
+    const backendapigw = new apigw.RestApi(this, 'BackendApigw', {
       restApiName: domainPrefixParam.valueAsString,
       defaultCorsPreflightOptions: {
         "allowOrigins": apigw.Cors.ALL_ORIGINS,
@@ -170,24 +200,47 @@ export class CdkAuthWebappStack extends Stack {
       }
     })
 
+    // This is the authorized to apply to the APIgw resource that check auth with the Cognito User Pool
     const backendapiauthorizer = new apigw.CognitoUserPoolsAuthorizer(this, 'BackendAPIAuthorizer', {
       cognitoUserPools: [userpool]
     })
 
-    const authorizedresource = backendapigw.root.addMethod('GET', lambdaapiintegration, {
+    // This is the lambda resource that define the API
+    const authorizedresource = backendapigw.root.addMethod('GET', lambdaapigwintegration, {
       authorizer: backendapiauthorizer,
       authorizationType: apigw.AuthorizationType.COGNITO
     })
 
+
+
+    /**
+     * FRONTEND DEPLOYMENT
+     * This is used to deploy the frontend code. The constants.js file is created upon deployment, it contains:
+     * domainPrefix - used to define the Cognito Hosted UI url
+     * apigw_id - used to define the APIGateway url
+     * region - used to define both the Cognito Hosted UI url and the APIGateway url
+     * cognito_client_id - supplied to the Hosted UI get request to perform authentication
+     */
     const s3deploymentfrontend = new s3deployment.BucketDeployment(this, 'DeployFrontEnd', {
       sources: [
         s3deployment.Source.asset('./frontend'),
-        s3deployment.Source.data('constants.js', `const constants = {domainPrefix:'${domainPrefixParam.valueAsString}', region:'${this.region}', cognito_client_id:'${client.userPoolClientId}', apigw_id:'${backendapigw.restApiId}'}`)
+        s3deployment.Source.data('constants.js', `
+          const constants = {
+            domainPrefix:'${domainPrefixParam.valueAsString}', 
+            region:'${this.region}', 
+            cognito_client_id:'${client.userPoolClientId}', 
+            apigw_id:'${backendapigw.restApiId}'
+          }`
+        )
       ],
-      destinationBucket: s3frontend,
+      destinationBucket: frontendbucket,
       distribution: cfdistribution
     })
 
+    /**
+     * END CONFIGURATION
+     * returning the url to reach the frontend
+     */
     new cdk.CfnOutput(this, 'YourPublicCloudFrontURL', {
       value: CLOUDFRONT_PUBLIC_URL,
       description: 'Navigate to the URL to access your deployed application'
